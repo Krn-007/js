8250fa18a2ea4c45c03c85cdd6db794c502b0e0f
Karanbeer Singh <131529676+Krn-007@users.noreply.github.com>
2024-07-23 13:46:33 +0000

    learning about stack and heap memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -49,3 +49,32 @@ const myFunction = function(){
 
 console.log(typeof myObj);
 console.log(typeof myFunction);
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive) , Heap(Non-Primitive)
+// whenever stack is selected a copy of original data is made and the operations are performed on it
+
+// example
+
+    let myName = "Karan"
+    let anotherName = myName
+    anotherName = "Lovely"
+
+   console.log(anotherName);
+   console.log(myName);
+
+
+// whenever heap is selected a reference of original data is provided and the changes are made to original data
+
+let userOne ={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+
+userTwo.email = "karan@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
