6f817efbab0934d4138b1cf342c1a34b72798b53
Karanbeer Singh <131529676+Krn-007@users.noreply.github.com>
2024-07-23 15:23:30 +0000

    learn about methods of number

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,11 @@
+const score = 400
+const balance = new Number(100)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 5225.9264
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
