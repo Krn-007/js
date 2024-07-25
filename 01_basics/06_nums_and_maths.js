b0d1592160e9b119466d4e61baffea4247ac86ea
Karanbeer Singh <131529676+Krn-007@users.noreply.github.com>
2024-07-25 10:25:02 +0000

    math and num

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,11 +1,29 @@
 const score = 400
 const balance = new Number(100)
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
 const otherNumber = 5225.9264
-console.log(otherNumber.toPrecision(4));
+//console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+//console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++MATHS+++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,6,8,6,4));
+// console.log(Math.max(4,6,7,8,9,6));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
