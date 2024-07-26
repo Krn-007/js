64de7b3fa73700847a8bdce1d1fb66c3ed1abb09
Karanbeer Singh <131529676+Krn-007@users.noreply.github.com>
2024-07-26 18:34:10 +0000

    learn about function and scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,4 +31,34 @@ function loginUserMessage(username = "sam") {
 }
 
 // console.log(loginUserMessage("karan"))
-console.log(loginUserMessage())
+//console.log(loginUserMessage())
+
+function calculateCartPrice(...num1){ // ... is a spread/rest operator which has functionality depend on the usecase here it used to gather all the data passed into the function and storing in the array
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500))
+
+const user = {
+    username: "karan",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "karan",
+    price: 399
+})
+
+const myNewArray = [200,400,500,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,500,600,300]));
