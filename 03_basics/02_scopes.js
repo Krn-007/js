9132374ec04d009a48dc0d315a5b26d5005023be
Karanbeer Singh <131529676+Krn-007@users.noreply.github.com>
2024-07-27 13:37:50 +0000

    scopes and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,8 +3,46 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("inner", a);
+    // console.log("inner", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
+
+function one(){
+    const username = "karan"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+if(true){
+    const username = "karan"
+    if(username === "karan"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++interesting+++++++++++++
+
+function addone(num){
+    return num+1
+}
+
+addone(5)
+
+const addTwo = function(num){
+    return num+2
+}
+
+addTwo(5)
